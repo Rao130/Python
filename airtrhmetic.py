0274a70840a961e9c8748f93f4cdef967050225b
Rao130 <raosahab87023@gmail.com>
2024-11-23 21:18:19 +0530

    airtrhmetic.py

---
+++ airtrhmetic.py
@@ -0,0 +1,8 @@
+@@ -0,0 +1,8 @@
+print("********Arithmetic operation********")
+first_no=32
+second_no=2.44
+print("Addition :",first_no+second_no)
+print("Subtraction :",first_no-second_no)
+print("Multiplication :",first_no*second_no)
+print("Division :",first_no/second_no)
